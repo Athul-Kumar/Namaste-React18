71dc29698cae0f51f3087c959f2f26b340c25524
Athul-Kumar <athulkumar7991@gmail.com>
2024-01-08 12:12:40 +0530

    chapter 02 files are created

---
+++ Chapter-02/script.js
@@ -0,0 +1,28 @@
+/**
+ * <div id="parent">
+ *      <div id="child1">
+ *            <h1>This is H1 tag</h1>
+ *            <h2>This is H2 tag</h2>
+ *      </div>
+ *      <div id="child2">
+ *            <h1>This is H1 tag</h1>
+ *            <h2>This is H2 tag</h2>
+ *      </div>
+ * </div>
+ */
+
+const parentTag = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "This is H1 tag"),
+    React.createElement("h2", {}, "This is H2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "This is H1 tag"),
+    React.createElement("h2", {}, "This is H2 tag"),
+  ]),
+]);
+
+console.log(parentTag);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parentTag);
