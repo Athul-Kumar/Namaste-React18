e4ea8131d155bd04f3484d7e4c424182ae1356af
Athul-Kumar <athulkumar7991@gmail.com>
2024-01-11 09:43:28 +0530

    chapter 05 project setup is finished

---
+++ Chapter-05/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+/**
+ *  Low level Planning for Restaurant APP
+ * 
+ * Header
+ *  -Logo
+ *  -Nav-list component
+ *      -Home
+ *      -About
+ *      -Cart
+ *      -contact
+ * Body
+ *  -search-input functionality
+ *  -Restaurant card components
+ *      -Img
+ *      -Restaurant name
+ *      -Cusines
+ *      -Delivery Time
+ *      -Price
+ *      -place
+ * 
+ * Footer
+ *  -copy right
+ *  -Links
+ *  -Adress
+ *  -Contact
+ * 
+ */
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render()
